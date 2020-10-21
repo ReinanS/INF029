15d7a4ff07e11c27f6e26b13ea6b387f70e1b974
Reinan Santos <reinandesouza01@gmail.com>
2020-10-21 13:23:11 -0300

    Protótipos Salvar e Recuperar Dados

---
+++ Trabalho3/Developer/main.c
@@ -0,0 +1,17 @@
+/*
+    **** To-do ****
+    [ ] - Salvar os dados que foram armazenados na estrutura principal
+    [ ] - Salvar os dados que foram armazenados nas estruturas auxiliares
+    [ ] - Ao abrir, dados devem ser lidos e armazenados
+    [ ] - Quando o usuário escolher a opcao sair, deve ser chamada a funcao para salvar os dados
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+	return 0;
+}
